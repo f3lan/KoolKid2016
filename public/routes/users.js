ae0a3b47061236cfdf1fdb616c8244886f1abcfe
Felix Langenegger <felix.langenegger@gmail.com>
2016-04-11 13:57:02 +0200

    New Front End Structure

---
+++ public/routes/users.js
@@ -0,0 +1,33 @@
+'use strict';
+
+(function() {
+
+  angular.module('MedEx').config(
+    [
+      '$stateProvider',
+      '$urlRouterProvider',
+      UserRoutes
+    ]);
+
+  function UserRoutes(
+    $stateProvider,
+    $urlRouterProvider
+  ) {
+
+    $urlRouterProvider.otherwise("/users/login");
+
+    $stateProvider
+      .state('login', {
+        url: '/users/login',
+        templateUrl: 'views/users/login.html'
+      })
+      .state('logout', {
+        url: '/users/logout',
+        templateUrl: 'views/users/logout.html'
+      })
+      .state('signup', {
+        url: '/users/sign-up',
+        templateUrl: 'views/users/signup.html'
+      });
+  }
+})();
